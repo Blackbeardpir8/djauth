8ed027e7ecf26bc5a57c777d9bc609c00e95c2d5
Blackbeardpir8 <deepakdk15yadav@gmail.com>
2024-08-09 13:43:02 +0530

    cutom user model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from accounts.managers import UserManager
+# Create your models here
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    username = None
+    name = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=12, unique=True)
+    parofile_image = models.ImageField(upload_to="profile",null=True,blank=True)
+    is_verified = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'phone_number'
+
+    REQUIRED_FIELDS = ['name',]
+
+    objects = UserManager()
